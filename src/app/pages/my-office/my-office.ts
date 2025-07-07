1764bac46ef4e99231ef4eddde87ac9e005f9c3c
yingchen <yingchen.meng@web.de>
2025-07-07 18:19:52 +0200

    Complete the basic layout of the feature card

--- src/app/pages/my-office/my-office.ts
+++ src/app/pages/my-office/my-office.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { HeroSection } from '../../shared/components/hero-section/hero-section';
+import { FeatureCard, FeatureIconType } from '../../shared/components/feature-card/feature-card';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-my-office',
-  imports: [HeroSection],
+  imports: [HeroSection, FeatureCard, TranslateModule],
   templateUrl: './my-office.html',
   styleUrl: './my-office.scss',
 })
-export class MyOffice {}
+export class MyOffice {
+  readonly iconAccounting: FeatureIconType = 'm-accounting';
+  readonly iconMarketing: FeatureIconType = 'm-marketing';
+  readonly iconUpgrade: FeatureIconType = 'm-upgrade';
+}
---
+++ src/app/shared/components/feature-card/feature-card.ts
@@ -0,0 +1,30 @@
+import { Component, input } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+export type FeatureIconType =
+  | 'm-accounting'
+  | 'm-marketing'
+  | 'm-upgrade';
+
+@Component({
+  selector: 'app-feature-card',
+  imports: [TranslateModule],
+  templateUrl: './feature-card.html',
+  styleUrl: './feature-card.scss'
+})
+export class FeatureCard {
+  icon = input<FeatureIconType>('m-accounting');
+  titleId = input<string>('FEATURE_CARD.ACCOUNTING.TITLE');
+
+  private iconMap: Record<FeatureIconType, string> = {
+    'm-accounting': 'assets/svgs/ic-buchhaltung.svg',
+    'm-marketing': 'assets/svgs/ic-marketing.svg',
+    'm-upgrade': 'assets/svgs/ic-upgrade.svg'
+  }
+
+  get iconPath(): string {
+    return this.iconMap[this.icon()] || this.iconMap['m-accounting'];
+  }
+
+
+}
